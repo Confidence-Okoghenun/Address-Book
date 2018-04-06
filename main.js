f6dc3c428bfe6cd5d8c1aab1d404861bf1cbb93a
Confidence Okoghenun <confidenceboi@gmail.com>
2018-04-06 17:05:35 +0100

    First Stable Version
    A little UI change was made and various bugs
    were fixed.
    
    *A 'hr' element was added to make it easy for
     users to differentiate between contacts in
     edit mode and floating details 'ul' was fixed.
    *The result of the above change has made the app
     more usable and bug free.
    *I honestly have no idea for future improvements
     right now :).
    
    [Closes-Bug: #006]

--- main.js
+++ main.js
@@ -2,22 +2,20 @@ var addContactButton = document.getElementById("buttonAdd");
 addContactButton.onclick = contactCreator;
 
 function contactCreator() {
-    
-    
     //Get all the contact detail's values such as first and last names
     //as well as phone number and store them in the blow variables
     var contactName = document.getElementById("Name").value;
     var eMail = document.getElementById("email").value;
     var phoneNumber = document.getElementById("phoneNumber").value;
-    
-  
-    //Create a new li bearing contact name in a paragraph element
-    //under the ul and give it the value of Name
+
+    //Create a new li bearing contact name in a nameListItemParagraph
+    //paragraph element under the ul and give it the value of Name
     var nameListItem = document.createElement("li");
     var nameListItemParagraph = document.createElement("p");
     var nameListItemContent = document.createTextNode(contactName);
     nameListItemParagraph.appendChild(nameListItemContent);
     
+    //Creates an img element and places it in nameListItemParagraph
     var nameListItemImage = document.createElement("img");
     nameListItemImage.setAttribute("src","img/arrow-down.png");
     nameListItemParagraph.appendChild(nameListItemImage);
@@ -33,7 +31,6 @@ function contactCreator() {
     var contactOrderedList = document.getElementById("contactList");
     nameListItem.appendChild(nameListItemParagraph);
     contactOrderedList.appendChild(nameListItem);
-    
     
     //
     //
@@ -68,7 +65,7 @@ function contactCreator() {
     detailsUnorderedList.setAttribute("hidden","");
     nameListItem.appendChild(detailsUnorderedList);
     
-    //Resets all input fields
+    //Resets all input fields after the addContactButton is clicked
     document.getElementById("contactForm").reset();
     
     //The below helps to hide or reveal contact details
@@ -93,127 +90,140 @@ function contactCreator() {
         }
     })();
     
-    
     //
     //
     //This portion is for editing the contact details
     //
     //
     
-    //this creates the editButton button in the detailsUnorderedList
+    //Creates the editButton button in the detailsUnorderedList
     var editButton = document.createElement("button");
     editButton.setAttribute("id", "edit-" + contactName);
     var editButtonText = document.createTextNode("Edit contact");
     editButton.appendChild(editButtonText);
     detailsUnorderedList.insertAdjacentElement("beforeend", editButton);
     
+    //Handles click event for the clickEditButton
     var clickEditButton = document.getElementById("edit-" + contactName);
     clickEditButton.onclick = editContactDetails;
     var counter2 = 1;
     
     function editContactDetails() {
         if(counter2%2 !== 0) {
+            //Gets all the contact detail's elements
             var nameToEdit = document.getElementById(contactName);
             var emailToEdit = document.getElementById(eMail);
             var phoneNumberToEdit = document.getElementById(phoneNumber);
             
+            //Creates a div for storing the contact form and the form
+            //itself
             var editDiv = document.createElement("div");
             editDiv.setAttribute("id", "edit-div-" + contactName);
             var editForm = document.createElement("form");
             
+            //Creates input field for editing the name
             var editNameTextInput = document.createElement("input");
             editNameTextInput.setAttribute("type","text");
             editNameTextInput.setAttribute("class","input-field");
             editNameTextInput.setAttribute("placeholder","enter new name");
             editNameTextInput.setAttribute("id","edit-" + contactName + "-text");
-            
-            
+
+            //Creates input field for editing the email
             var editEmailTextInput = document.createElement("input");
             editEmailTextInput.setAttribute("type","text");
             editEmailTextInput.setAttribute("class","input-field");
             editEmailTextInput.setAttribute("placeholder","enter new email");
             editEmailTextInput.setAttribute("id","edit-" + eMail + "-text");
             
+            //Creates input field for editing the phone number
             var editPhoneNumberTextInput = document.createElement("input");
             editPhoneNumberTextInput.setAttribute("type","text");
             editPhoneNumberTextInput.setAttribute("class","input-field");
             editPhoneNumberTextInput.setAttribute("placeholder","enter new phone number");
             editPhoneNumberTextInput.setAttribute("id","edit-" + phoneNumber + "-text");
             
+            //Creates the save button
             var saveButton = document.createElement("input");
             saveButton.setAttribute("type","submit");
             saveButton.setAttribute("value","Save");
             saveButton.setAttribute("id","save-button-" + contactName);
             
-            
-            
+            //Creates span for edit name with 'Name' as value
             var spanForEditName = document.createElement("span");
             var textSpanForEditName = document.createTextNode("Name");
             spanForEditName.appendChild(textSpanForEditName);
             
+            //Creates span for edit email with 'eamil' as value
             var spanForEditEmail = document.createElement("span");
             var textSpanForEditEmail = document.createTextNode("Email");
             spanForEditEmail.appendChild(textSpanForEditEmail);
             
+            //Creates span for edit phone number with 'phone number' as
+            //value
             var spanForPhoneNumber = document.createElement("span");
             var textSpanForPhoneNumber = document.createTextNode("Phone Number");
             spanForPhoneNumber.appendChild(textSpanForPhoneNumber);
             
-            
-            
+            //Creates labels to put the input inside
             var labelForEditName = document.createElement("label");
             var labelForEditEmail = document.createElement("label");
             var labelForPhoneNumber = document.createElement("label");
             
+            //Places the span into their respective labels
             labelForEditName.appendChild(spanForEditName);
             labelForEditEmail.appendChild(spanForEditEmail);
             labelForPhoneNumber.appendChild(spanForPhoneNumber);
             
+            //Places the inputs into their respective labels
             labelForEditName.appendChild(editNameTextInput);
             labelForEditEmail.appendChild(editEmailTextInput);
             labelForPhoneNumber.appendChild(editPhoneNumberTextInput);
             
+            //Places all labels into the editForm element
             editForm.appendChild(labelForEditName);
             editForm.appendChild(labelForEditEmail);
             editForm.appendChild(labelForPhoneNumber);
             
+            //Places editDiv after the clickEditButton and editForm
             clickEditButton.insertAdjacentElement('afterend', editDiv);
             editDiv.appendChild(editForm);
             editForm.insertAdjacentElement('afterend',saveButton);
             
+            //Controls evet for clickToSave button
             var clickToSave = document.getElementById("save-button-" + contactName);
             clickToSave.onclick = editAndSave;
                     
             function editAndSave() {
-                
-                
+                //Sets name to newly edited value
                 var nameToSave = document.getElementById("edit-" + contactName + "-text").value;
                 if(nameToSave === "") {} else {
                 document.getElementById(contactName).innerHTML = nameToSave;
                 }
                 
-                
+                // Sets email to newly edited value
                 var eMailToSave = document.getElementById("edit-" + eMail + "-text").value;
                 if(eMailToSave === "") {} else {
                 document.getElementById(eMail).innerHTML = "Email: " + eMailToSave;
                 }
                 
-                
+                //Sets phone number to newly edited value
                 var phoneNumberToSave = document.getElementById("edit-" + phoneNumber + "-text").value;
                 if(phoneNumberToSave === "") {} else {
                 document.getElementById(phoneNumber).innerHTML = "Tel: " + phoneNumberToSave;
                 }
                 
-                
+                //After which it hides the editDiv
                 var hideEditDiv = document.getElementById("edit-div-" + contactName);
                 hideEditDiv.remove();
                 
                 return counter2--;
             }
         } else {
+            //Hides the edit div if clicks are even
             var hideEditDiv = document.getElementById("edit-div-" + contactName);
             hideEditDiv.remove();
         }
+        
         return counter2++;
     }
     
@@ -223,17 +233,24 @@ function contactCreator() {
     //
     //
     
+    //Creates the delete button and places it as last child of detailsUnorderedList
     var deleteButton = document.createElement("button");
     deleteButton.setAttribute("id", "delete-" + contactName);
     var deleteButtonText = document.createTextNode("Delete contact");
     deleteButton.appendChild(deleteButtonText);
     detailsUnorderedList.insertAdjacentElement("beforeend", deleteButton);
     
+    //Handles click event for delete button
     var clickDeleteButton = document.getElementById("delete-" + contactName);
     clickDeleteButton.onclick = deleteContactDetails;
     
     function deleteContactDetails() {
+        //Deletes contact
         var allContactDetails = document.getElementById("contact-" + contactName);
         allContactDetails.remove();
     }
+    
+    //Creates horizontal rule below the contact details
+    var nameListItemHr = document.createElement("hr");
+    deleteButton.insertAdjacentElement("afterend", nameListItemHr);
 }
